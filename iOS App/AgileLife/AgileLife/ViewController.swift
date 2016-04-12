f6db84f725856155e6178ee64d5474e992de126a
Zachary Gover <zachary1748@gmail.com>
2016-04-11 20:45:34 -0500

    Added home screen and implemented side menu functionality with sample data in the header.

--- iOS App/AgileLife/AgileLife/ViewController.swift
+++ iOS App/AgileLife/AgileLife/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var menuBtn: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.setDefualtNav(menuBtn)
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ iOS App/AgileLife/AgileLife/ViewControllerExtensions.swift
@@ -0,0 +1,41 @@
+//
+//  ViewControllerExtensions.swift
+//  AgileLife
+//
+//  Created by Zachary Gover on 4/11/16.
+//  Copyright © 2016 Full Sail University. All rights reserved.
+//
+
+import Foundation
+
+extension UIViewController {
+    
+    /* ==========================================
+    *
+    * Change the default properties for the nav
+    *
+    * =========================================== */
+    func setDefualtNav(menuBtn: UIBarButtonItem) {
+
+        // Create status bar bg
+        let view = UIView(frame: CGRect(x: 0.0, y: 0.0, width: UIScreen.mainScreen().bounds.size.width, height: 20.0))
+        view.backgroundColor = UIColor(red: 0/255, green: 55/255, blue: 53/255, alpha: 0.5)
+        
+        // Make sure the nav controller won't go beyond the status bar so we may see the status bar bg
+        self.navigationController?.navigationBar.clipsToBounds = true
+        
+        // Set the application BG and add the status bar background
+        self.view.backgroundColor = UIColor(red: 36/255, green: 103/255, blue: 101/255, alpha: 1)
+        self.view.addSubview(view)
+        
+        // Open the side menu by the bar button item
+        if revealViewController() != nil {
+            let deviceWidth = UIScreen.mainScreen().bounds.size.width
+            revealViewController().rearViewRevealWidth = (80 / 100) * deviceWidth;
+            
+            menuBtn.target = revealViewController()
+            menuBtn.action = "revealToggle:"
+        }
+    }
+    
+}
